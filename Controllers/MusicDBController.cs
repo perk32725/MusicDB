aeb22c393c3dc2476270f93a8b4867601bd73b46
Bill Perkins <wrp@Number6.local>
2018-11-22 14:30:26 -0500

    After scaffolding

--- Controllers/MusicDBController.cs
+++ Controllers/MusicDBController.cs
@@ -1,33 +1,149 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC1.Models;
 
 namespace MVC1.Controllers
 {
     public class MusicDBController : Controller
     {
-        /* sample:
-        // GET: /<controller>/
-        public IActionResult Index() {
-            return View();
-        }
-        */
+        readonly MusicDBContext _context;
 
-        // GET: /MusicDB/
-        public IActionResult Index()
+        public MusicDBController(MusicDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MusicDB
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Album.ToListAsync());
+        }
+
+        // GET: MusicDB/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var musicDB = await _context.Album
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (musicDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(musicDB);
+        }
+
+        // GET: MusicDB/Create
+        public IActionResult Create()
         {
             return View();
         }
 
-        // GET: /MusicDB/Catalog/
-        public IActionResult Catalog()
+        // POST: MusicDB/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Album,Artist")] MusicDB musicDB)
         {
-            ViewData["MainMsg"] = "ViewData['MainMsg']: from Controllers/MusicDBController.cs";
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(musicDB);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(musicDB);
+        }
+
+        // GET: MusicDB/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var musicDB = await _context.Album.FindAsync(id);
+            if (musicDB == null)
+            {
+                return NotFound();
+            }
+            return View(musicDB);
+        }
+
+        // POST: MusicDB/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Album,Artist")] MusicDB musicDB)
+        {
+            if (id != musicDB.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(musicDB);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MusicDBExists(musicDB.ID))
+                    {
+                        return NotFound();
+                    }
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(musicDB);
+        }
+
+        // GET: MusicDB/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var musicDB = await _context.Album
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (musicDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(musicDB);
+        }
+
+        // POST: MusicDB/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var musicDB = await _context.Album.FindAsync(id);
+            _context.Album.Remove(musicDB);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        bool MusicDBExists(int id)
+        {
+            return _context.Album.Any(e => e.ID == id);
         }
     }
 }
