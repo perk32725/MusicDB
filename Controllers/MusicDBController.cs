ee0b1ff7e6930d6a456cefa61cc15d53dfa039b5
Bill Perkins <wrp@Number6.local>
2018-11-22 10:08:05 -0500

    Initial commit

---
+++ Controllers/MusicDBController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC1.Controllers
+{
+    public class MusicDBController : Controller
+    {
+        /*
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        // GET: /MusicDB/
+
+        //public String Index()
+        public IActionResult Index()
+        {
+            /* Normally you would declare a method returning an iActionResult,
+             * such as we see above. The normal action is to return View();
+             */
+            /*
+            return "This is the default action in MusicDBController, \n" +
+                "It is from /MusicDB.";
+             */
+            return View();
+        }
+
+        // GET: /MusicDB/Catalog/
+        public String Catalog()
+        {
+            return "This is from /MusicDB/Catalog.";
+        }
+    }
+}
