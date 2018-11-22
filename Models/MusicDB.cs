8c13d0c9ef1c5ca00b153932fcb39760cf0c5ca5
Bill Perkins <wrp@Number6.local>
2018-11-22 14:29:54 -0500

    Initial commit

---
+++ Models/MusicDB.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC1.Models
+{
+    public class MusicDB
+    {
+        public int ID { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+    }
+}
---
+++ Models/MusicDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC1.Models
+{
+    public class MusicDBContext : DbContext
+    {
+        public MusicDBContext(DbContextOptions<MusicDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC1.Models.MusicDB> Album { get; set; }
+        public DbSet<MVC1.Models.MusicDB> Artist { get; set; }
+
+    }
+}
